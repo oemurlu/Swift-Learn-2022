88067b93b2a043af90f1e4ae423d42f33a148943
oemurlu <oemurlu@gmail.com>
2022-08-31 17:04:19 +0300

    create Exercises

---
+++ Exercises/1-Variables.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+// ------ PART 1 ------
+
+// Variables & Constants (degiskenler & sabitler)
+// var = variable
+// let = constant
+
+var greeting = "Hello, playground"
+var myNumber = 3 * 13
+print(myNumber)
+
+//shift enter ile run ediyoruz ya da satir basindaki play isaretinden bastan istedigimiz koda kadar calistiriyoruz
+
+var userName = "oe"
+print(userName)
+userName = "osman emre"
+print(userName)
+var userSurname = "omurlu"
+
+let dogumYili = 2002
+//dogumYili = 1997 yapamayiz cunku degisken degil sabit.
+print(dogumYili)
+
+var myName = "osman emre"
+myName.append("berg")
+myName.uppercased()
+myName.lowercased()
+
+//integer
+let myAge = 50
+let number = 4
+myAge / number
+
+//double
+let myAgeD = 50.0
+let numberD = 4.0
+myAgeD / numberD
+
+//boolean
+var myBoolean = false
+myBoolean = true
+
+// ------ PART 2 ------
+//degisken tipini onceden belirleme
+
+let myString : String = "50"
+let myString2 : String = "yamaha"
+
+//let stringNumber1 : String = 10 //boyle olmaz cunku string gircem diyip int giriyosun.
+let stringNumber2 : String = String(20)
+
+
+//simdi; define etmeden initialize edemeyiz
+//define
+let myVariable : String
+
+//initialization
+myVariable = "Gizem"
+//myVariable.uppercased()
+
+let myUpperVariable = myVariable.uppercased()
+
+
+
+
+
+
+
