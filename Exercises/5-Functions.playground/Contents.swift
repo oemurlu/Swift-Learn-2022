bf2c758ebadaa03240a6aaeb77e774184d22e97c
oemurlu <oemurlu@gmail.com>
2022-09-02 11:46:03 +0300

    create 5-Functions

--- Exercises/5-Functions.playground/Contents.swift
+++ Exercises/5-Functions.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func myFunction(){
+    print("my function printed")
+}
+myFunction()
+
+
+// Input & Output & Return yapisi
+
+//void function
+func sum(x: Int, y: Int){
+    print(x + y)
+}
+sum(x: 10, y: 20) // output 30 // sum yazarken void yaziyo zaten orda.
+
+//return function
+func minus(a: Int, b: Int) -> Int {
+    return a-b
+}
+minus(a: 8, b: 4) // minus yazarken int yaziyo artik orda. yani bunu bir int degisken olarak atayabiliriz.
+let degisken = minus(a: 10, b: 3)
+print(degisken)
+
+func logicFunc(a: Int, b: Int) -> Bool {
+    if a > b{
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+logicFunc(a: 10, b: 2) // true
+logicFunc(a: 2, b: 10) // false
+
