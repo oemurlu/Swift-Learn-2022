0a95e4d676e51462b4dfff70f47d70f584ff1a2a
oemurlu <oemurlu@gmail.com>
2022-09-02 11:24:55 +0300

    create 4-ifStatements

---
+++ Exercises/4-ifStatements.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var myAge = 32
+
+if myAge < 30 {
+    print("30 -")
+}
+else if myAge > 30 && myAge < 40 {
+ print("30s")
+}
+else {
+    print("40 +")
+}
+
+// && AND
+// || OR
+
+3 < 5 && 5 < 7 // true
+3 < 5 || 5 < 3 // true
