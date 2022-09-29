d18c3721a7e1f9ffefdc11a2651f44ced205089b
oemurlu <oemurlu@gmail.com>
2022-09-29 19:48:43 +0300

    Create 001-I-Am-Rich-App

---
+++ Udemy-Dr.Angela_Yu/001-I-Am-Rich-App/001-I-Am-Rich-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  001-I-Am-Rich-App
+//
+//  Created by Osman Emre Ömürlü on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
