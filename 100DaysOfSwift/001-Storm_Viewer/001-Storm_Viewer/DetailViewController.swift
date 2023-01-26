9f42bcd183093186feff3419e66946bd94008602
oemurlu <oemurlu@gmail.com>
2023-01-26 17:22:25 +0300

    Create 001-Storm_Viewer

---
+++ 100DaysOfSwift/001-Storm_Viewer/001-Storm_Viewer/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  001-Storm_Viewer
+//
+//  Created by Osman Emre Ömürlü on 24.01.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+        
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+        
+    }
+}
---
+++ 100DaysOfSwift/001-Storm_Viewer/001-Storm_Viewer/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  001-Storm_Viewer
+//
+//  Created by Osman Emre Ömürlü on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                //this is a picture to load!
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = pictures[indexPath.row]
+        cell.contentConfiguration = content 
+        return cell
+    }
+    
+    //segue ile baglanti yerine kodla baglanti yapacagiz.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
+
+
+
+
+
+
+
